7b80b58a41ff706eb805be602052b3ce35096642
zhuzewei <755199443@qq.com>
2019-10-28 21:19:28 +0800

    Initial commit

---
+++ src/main/scala/com/hive/ETLUtils.scala
@@ -0,0 +1,13 @@
+package com.hive
+
+/**
+  * @author 朱同学
+  *         date 2019/10/28
+  *         description HiveProject com.hive
+  *         version 1.0
+  */
+object ETLUtils {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
